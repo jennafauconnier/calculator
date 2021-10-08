31cda509ce7176e4084b81cc37d6d8fc3f9291a0
Jenna <jenna.fauconnier@gmail.com>
2021-10-08 16:18:41 +0200

    js script about point a button and check the value

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ let newNumb = parseInt(stringInt, 10)
 
 
 console.log(newNumb + 4)
+
+let tagName = document.querySelectorAll('*[id]');
+
+let transformToArray = Array.from(tagName);
+
+function getBtnClicked() {
+    let getOnlyNumb = transformToArray.slice(1);
+    return getOnlyNumb;
+}
+
+console.log(getBtnClicked());
